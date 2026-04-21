fe90dd1da042a27600840cbfc1aac30d62af13d9
Hamza <azri.hamza@gmail.com>
2026-04-21 13:26:48 +0100

    feat: add auth guard to protect users routes

--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -3,9 +3,10 @@ import { App } from './app';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { EditUserComponent } from './users/edit/edit-user.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'users', component: EditUserComponent },
+  { path: 'users', component: EditUserComponent, canActivate: [authGuard] },
 ];
